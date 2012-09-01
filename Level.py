a3d78227635c29c3de8962e9be92a11413178b2b
Stephen Eckenrode <stephen.eckenrode@gmail.com>
2012-08-31 20:25:22 -0400

    Level Stub

---
+++ Level.py
@@ -0,0 +1,23 @@
+class Level:
+	def __init__(self, kernel):
+		self.mRawLevelEntities = []
+		self.mLevelEntities = []
+		self.mCollidableEntities = []
+
+		return
+
+	def LoadLevel(self, levelname):
+
+		return
+
+	def ProcessEntities(self):
+
+		return
+		
+	def Update(self, delta):
+
+		return
+
+	def Draw(self):
+
+		return
