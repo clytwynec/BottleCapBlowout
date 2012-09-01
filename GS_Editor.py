29e8e6623508bb4bf4799d2c2783a6480efb4a0c
Stephen Eckenrode <stephen.eckenrode@gmail.com>
2012-08-31 22:33:13 -0400

    Adding Editor/Game Base

---
+++ GS_Editor.py
@@ -0,0 +1,43 @@
+import pygame
+import os
+import math
+import Tkinter
+import tkMessageBox
+
+from GameState import *
+from Level import *
+from pygame.locals import *
+
+class GS_Editor(GameState):
+	def __init__(self, kernel, gsm):
+		GameState.__init__(self, "Editor", kernel, gsm)
+
+	def Initialize(self):
+		Tkinter.Tk().wm_withdraw()
+		levelName = tkMessageBox.askstring("New Level", "Enter a Level Name:");
+
+		print levelName
+
+		return GameState.Initialize(self)
+
+	def Destroy(self):
+		return GameState.Destroy(self)
+
+	def Pause(self):
+
+		return GameState.Pause(self)
+
+	def Unpause(self):
+
+		return GameState.Unpause(self)
+
+	def HandleEvent(self, event):
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+		return GameState.HandleEvent(self, event)
+
+	def Update(self, delta):
+		
+		return GameState.Update(self, delta)
---
+++ Game.py
@@ -0,0 +1,70 @@
+###################################################################################
+# main.py
+#
+# The kickoff script/main loop.  Sets up the different game systems, and then
+# enters the main loop.  Runs until we receive a system event to quit the game
+#
+# Command Line Arguments
+###################################################################################
+
+# System level imports
+from optparse import OptionParser
+import sys
+import os
+import math
+import pygame
+import random
+
+from pygame.locals import *
+
+# App level imports
+from GameKernel import *
+from GameStateManager import *
+from GS_Editor import *
+
+#random.seed(0)
+
+
+#########################
+# Start Main
+#########################
+
+#### Parse command line arguments
+optionParser = OptionParser()
+
+#### Kick off the graphics/window system
+kernel = GameKernel()
+screenSurface = kernel.InitializeDisplay((800, 600))
+ticker = kernel.Ticker()
+
+#### Initialize game states
+gsm = GameStateManager()
+gsm.RegisterState(GS_Editor(kernel, gsm))
+
+gsm.SwitchState("Editor")
+
+font = pygame.font.SysFont("Helvetica", 12)
+
+## Main Loop
+while (1):
+
+	delta = ticker.get_time()
+
+	FPSSurf = font.render("FPS: " + str(int(ticker.get_fps())), True, (255, 255, 255))
+	FPSRect = FPSSurf.get_rect()
+	FPSRect.topright = screenSurface.get_rect().topright
+	screenSurface.blit(FPSSurf, FPSRect)
+
+	gsm.Update(delta)
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+		else:
+			gsm.GetActiveState().HandleEvent(event)
+
+	kernel.FlipDisplay()
+
+	ticker.tick()
+	
