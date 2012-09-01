ea8ba7dfdd34357789c263c615e1b7358d83a3b9
Stephen Eckenrode <stephen.eckenrode@gmail.com>
2012-09-01 17:25:39 -0400

    Adding Files

---
+++ GS_MainMenu.py
@@ -0,0 +1,26 @@
+from GS_MenuBase import *
+from Level import *
+
+class GS_MainMenu(GS_MenuBase):
+	def __init__(self, kernel, gsm):
+		GS_MenuBase.__init__(self, "MainMenu", kernel, gsm)
+
+		self.mLevel = Level(kernel, 800)
+		self.mLevel.LoadLevel("MainMenu.lvl")
+
+		self.mHeading, self.mHeadingRect = kernel.ImageManager().LoadImage("heading.bmp")
+		self.mHeadingRect.topleft = (400 - self.mHeadingRect.width / 2, 50)
+
+		self.mMenuImages["Game"], self.mMenuRects["Game"] = kernel.ImageManager().LoadImage("newgame.bmp")
+		self.mMenuRects["Game"].topleft = (400 - self.mMenuRects["Game"].width / 2, 200)
+
+		self.mMenuImages["Tutorial"], self.mMenuRects["Tutorial"] = kernel.ImageManager().LoadImage("tutorial.bmp")
+		self.mMenuRects["Tutorial"].topleft = (400 - self.mMenuRects["Tutorial"].width / 2, 300)
+
+		self.mMenuImages["Exit"], self.mMenuRects["Exit"] = kernel.ImageManager().LoadImage("exit.bmp")
+		self.mMenuRects["Exit"].topleft = (400 - self.mMenuRects["Exit"].width / 2, 400)
+
+	def Update(self, delta):
+		self.mLevel.Draw()
+
+		GS_MenuBase.Update(self, delta)
---
+++ GS_MenuBase.py
@@ -0,0 +1,64 @@
+import pygame
+import sys
+import os
+
+from GameState import *
+from GameKernel import *
+import Colors
+
+from pygame.locals import *
+
+class GS_MenuBase(GameState):
+	def __init__(self, name, kernel, gsm):
+		GameState.__init__(self, name, kernel, gsm)
+
+		self.mHeading = None
+		self.mHeadingRect = None
+		self.mMenuItems = {}
+		self.mMenuImages = {}
+		self.mMenuImagesHover = {}
+		self.mMenuRects = {}
+
+	def Initialize(self):
+
+		return GameState.Initialize(self)
+
+	def Destroy(self):
+		return GameState.Destroy(self)
+
+	def Pause(self):
+		self.mMenuItems = {}
+		self.mHeading = None
+
+		return GameState.Pause(self)
+
+	def Unpause(self):
+		self.Initialize()
+
+		return GameState.Unpause(self)
+
+	def HandleEvent(self, event):
+		if event.type == MOUSEMOTION:
+			for item in self.mMenuRects:
+				if (self.mMenuRects[item].collidepoint(event.pos) and item in self.mMenuImagesHover):
+					self.mMenuItems[item] = self.mMenuImagesHover[item]
+				else:
+					self.mMenuItems[item] = self.mMenuImages[item]
+		elif event.type == MOUSEBUTTONDOWN:
+			for item in self.mMenuRects:
+				if (self.mMenuRects[item].collidepoint(event.pos)):
+					if (item == "Exit"):
+						pygame.quit()
+						sys.exit()
+					else:
+						self.mGameStateManager.SwitchState(item)
+
+
+	def Update(self, delta):
+		if (self.mHeading):
+			self.mKernel.DisplaySurface().blit(self.mHeading, self.mHeadingRect)
+
+		for item in self.mMenuItems:
+			self.mKernel.DisplaySurface().blit(self.mMenuItems[item], self.mMenuRects[item])
+
+		return GameState.Update(self, delta)
